47fc7f17957e46acb2fb2374ddb8b0dcbaab48a3
Ipatii Navarro <ipatiinavarro@gmail.com>
2024-12-09 15:26:03 +0300

    1 line for unique words

--- HomeWork/10.12/src/main/java/org/example/UniqueWords.java
+++ HomeWork/10.12/src/main/java/org/example/UniqueWords.java
@@ -14,11 +14,6 @@ public class UniqueWords {
     }
 
     public int countOfWords() throws IOException {
-        HashSet<String> words = new HashSet<>(); words.add("");
-        Scanner sc = new Scanner(new File(this.filePath));
-        while (sc.hasNextLine())
-            words.addAll(Arrays.stream((sc.nextLine().toLowerCase().trim().split("\\W"))).toList());
-
-        return words.size()-1;
+        HashSet<String> words = new HashSet<>(); words.add(""); Scanner sc = new Scanner(new File(this.filePath)); while (sc.hasNextLine()) words.addAll(Arrays.stream((sc.nextLine().toLowerCase().trim().split("\\W"))).toList()); return words.size()-1;
     }
 }
